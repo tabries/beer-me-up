d55c8757d5be2a6661aaaaf119f0634ea1b0eb8f
Gabriel Martínez <gamartinezayala@gmail.com>
2024-05-29 12:38:24 -0400

    Add 2nd comment to test release-drafter

--- src/index.js
+++ src/index.js
@@ -16,4 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // Test comment 001
+// Test comment 002
 reportWebVitals();
